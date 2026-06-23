2435e41fb886447ce9a2de9d9967e9f6bab59a7c
SportWeb Developer <developer@sportweb.local>
2026-06-23 17:56:02 +0530

    ...

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,11 +23,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ApexSport');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ApexSport');
-  });
 });
